f7d075876b83fe5d383fc5bce9aa3eefd7dc6059
Todd Leslie <spotontechnology@gmail.com>
2020-05-27 20:26:05 -0600

    commit

---
+++ db/seed.sql
@@ -0,0 +1,33 @@
+use employees;
+
+INSERT INTO department
+    (name)
+VALUES
+    ('Sales'),
+    ('Engineering'),
+    ('Finance'),
+    ('Legal');
+
+INSERT INTO role
+    (title, salary, department_id)
+VALUES
+    ('Sales Lead', 100000, 1),
+    ('Salesperson', 80000, 1),
+    ('Lead Engineer', 150000, 2),
+    ('Software Engineer', 120000, 2),
+    ('Account Manager', 160000, 3),
+    ('Accountant', 125000, 3),
+    ('Legal Team Lead', 250000, 4),
+    ('Lawyer', 190000, 4);
+
+INSERT INTO employee
+    (first_name, last_name, role_id, manager_id)
+VALUES
+    ('John', 'Doeboy', 1, NULL),
+    ('Jackie', 'Chan', 2, 1),
+    ('Scarlett', 'Johansson', 3, NULL),
+    ('Robert', 'Downey', 4, 3),
+    ('Toby', 'Mcquire', 5, NULL),
+    ('Bob', 'Barker', 6, 5),
+    ('Bill', 'Maher', 7, NULL),
+    ('Tim', 'Allen', 8, 7);
